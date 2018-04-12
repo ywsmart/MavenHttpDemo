d5c4c369b872275109c5cfe0bac69d15e05325cc
ywsmart <ywsmart@163,com>
2018-04-13 00:38:33 +0800

    新增用例失败重试功能

--- src/test/java/com/yvan/listener/TestngRetry.java
+++ src/test/java/com/yvan/listener/TestngRetry.java
@@ -1,8 +1,38 @@
 package com.yvan.listener;
 
+import com.yvan.exception.BaseException;
+import org.testng.IRetryAnalyzer;
+import org.testng.ITestResult;
+import org.testng.Reporter;
+
 /**
- * Function：
+ * Function：覆写重试分析接口
  * Created by yawa1hz1 on 2018/4/12 18:03.
  */
-public class TestngRetry {
+public class TestngRetry implements IRetryAnalyzer {
+
+    private static int retryCount = 1;
+    private static int maxRetryCount = 2;
+
+    public boolean retry(ITestResult result) {
+        // TODO Auto-generated method stub
+        if (result.getThrowable() instanceof BaseException && retryCount % maxRetryCount != 0) {
+//			String message = "running retry for  '" + result.getName()
+//					+ "' on class " + this.getClass().getName() + " Retrying "
+//					+ retryCount + " times";
+            Reporter.setCurrentTestResult(result);
+
+            Reporter.log("RunCount=" + (retryCount + 1));
+            retryCount++;
+            return true;
+        } else {
+            resetRetryCount();
+            return false;
+        }
+    }
+
+    public static void resetRetryCount() {
+        retryCount = 1;
+    }
+
 }
