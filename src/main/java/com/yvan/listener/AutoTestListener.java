64f26ec0be965d402fd06da0c6355d3230e0d1e3
ywsmart <ywsmart@163.com>
2020-05-18 22:43:40 +0800

    优化部分代码

--- src/main/java/com/yvan/listener/AutoTestListener.java
+++ src/main/java/com/yvan/listener/AutoTestListener.java
@@ -92,8 +92,7 @@ public class AutoTestListener extends TestListenerAdapter {
     private int getId(ITestResult result) {
         int id = result.getTestClass().getName().hashCode();
         id = id + result.getMethod().getMethodName().hashCode();
-        id = id
-                + (result.getParameters() != null ? Arrays.hashCode(result
+        id = id + (result.getParameters() != null ? Arrays.hashCode(result
                 .getParameters()) : 0);
         return id;
     }
